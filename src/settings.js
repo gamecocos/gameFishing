ed13e26e06567986bcb8991b2bdb12b6d237a93e
VC_Sophea <142644660+gamecocos@users.noreply.github.com>
2023-10-09 08:32:01 +0700

    banca

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","wall","movegrid","tilemap"],collisionMatrix:[[true],[false,false,true,false],[false,true,false,false],[false,false,false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/FishSingle/scene/LoginScene.fire",orientation:"landscape",jsList:[]};
